de40aa8db0afa86c1b9338e783b678ef083c360e
Andrew <andrew.muloki@strathmore.edu>
2023-04-11 19:29:59 +0300

    create expense cards

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 13.35,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Pizza Hut',
+      amount: 9.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
